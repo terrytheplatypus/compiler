3c212d1b05aa01052ce70af7897bfc00777a9075
david <aliasup@yahoo.com>
2018-10-06 18:48:19 -0400

    runtime basic

--- src/runtime.c
+++ src/runtime.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 
+
 void printint(int x) {
-setbuf(stdout, NULL);
-fprintf(stdout,"%d\n",x);
-setbuf(stdout, 0);
-fflush(stdout);
+printf("answer is %d\n", x);
+return;
 }
+
 int readint(){
 int n;
 scanf("%d", &n);
